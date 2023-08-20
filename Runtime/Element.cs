d7720895c11dd15d3bab179bd30ee0095719a7d0
Daniel Artmann <daniel@artmann.co>
2023-08-20 12:39:27 +0200

    Feat: Builder pattern for element.

--- Runtime/Element.cs
+++ Runtime/Element.cs
@@ -2,6 +2,20 @@ namespace Paper
 {
     public abstract class Element
     {
+        public string background = "inherit";
+        public string backgroundColor = "inherit";
+
+        // Position
+        public string position = "relative";
+        public string bottom = "auto";
+        public string left = "auto";
+        public string right = "auto";
+        public string top = "auto";
+
+        // Sizing
+        public string height = "auto";
+        public string width = "auto";
+        
         public Element[] children;
 
         public Element(params Element[] children)
--- Runtime/Elements/Box.cs
+++ Runtime/Elements/Box.cs
@@ -2,21 +2,9 @@ namespace Paper
 {
     public class Box : Element
     {
-        public string background = "inherit";
-        public string backgroundColor = "inherit";
-
-        // Position
-        public string position = "relative";
-        public string bottom = "auto";
-        public string left = "auto";
-        public string right = "auto";
-        public string top = "auto";
-
-        // Sizing
-        public string height = "auto";
-        public string width = "auto";
-
         public Box(params Element[] children)
-            : base(children) { }
+            : base(children)
+        {
+        }
     }
 }
