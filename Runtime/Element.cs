6c0e383918b9fb154cce1ac2123f66635acf45bc
Daniel Artmann <daniel@artmann.co>
2023-08-20 12:47:35 +0200

    Fix: Moved builder to element class.

--- Runtime/Element.cs
+++ Runtime/Element.cs
@@ -15,12 +15,66 @@ namespace Paper
         // Sizing
         public string height = "auto";
         public string width = "auto";
-        
+
         public Element[] children;
 
         public Element(params Element[] children)
         {
             this.children = children;
+        }
+
+        public Element Position(string position)
+        {
+            this.position = position;
+            return this;
+        }
+
+        public Element Bottom(string bottom)
+        {
+            this.bottom = bottom;
+            return this;
+        }
+
+        public Element Left(string left)
+        {
+            this.left = left;
+            return this;
+        }
+
+        public Element Right(string right)
+        {
+            this.right = right;
+            return this;
+        }
+
+        public Element Top(string top)
+        {
+            this.top = top;
+            return this;
+        }
+
+        public Element Height(string height)
+        {
+            this.height = height;
+            return this;
+        }
+
+        public Element Width(string width)
+        {
+            this.width = width;
+            return this;
+        }
+
+        public Element Background(string background)
+        {
+            this.background = background;
+            return this;
+        }
+
+        public Element BackgroundColor(string backgroundColor)
+        {
+            this.backgroundColor = backgroundColor;
+            return this;
         }
     }
 }
